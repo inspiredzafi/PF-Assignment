bf357fb6645a94425d97bc42e47f83e480618d07
Muhammad Huzaifa Ansar <huzaifaofficial087@gmail.com>
2025-11-19 19:46:51 +0500

    Computed the product of two matrices using raw Logic. The next step is to figure out the pattern and implement it using a loop

---
+++ 2D Array/2darray.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+    
+    // 2D Array
+
+    int rows = 2;
+    int cols = 2;
+    
+    // Matrix 1
+    int M1[rows][cols] = {
+        {1, 2}, 
+        {3, 4}
+    };
+
+    // Matrix 2
+    int M2[rows][cols] = {
+        {3, 4},
+        {5, 6}
+    };
+
+    // Product of the matrices would be stored in this matrix.
+    int M[rows][cols];
+
+    M[0][0] =   M1[0][0] * M2[]
+    M[0][1] =
+    M[1][0] =
+    M[1][1] =
+
+    
+    return 0;
+
+}
