ec3e5546c6b77fd01ca420ceb0654314bc691234
Muhammad Huzaifa Ansar <huzaifaofficial087@gmail.com>
2025-11-19 20:22:15 +0500

    Computed Addition and Subtraction of the Arrays and printed them

--- 2D Array/2darray.cpp
+++ 2D Array/2darray.cpp
@@ -23,24 +23,96 @@ int main(){
     };
 
     // Product of the matrices would be stored in this matrix.
-    int M[rows][cols];
+    int MP[rows][cols];         // MP - Matrix storing the product of M1 and M2.
 
-    M[0][0] =   M1[0][0] * M2[0][0] + M1[0][1] * M2[1][0] ;
-    M[0][1] =   M1[0][0] * M2[0][1] + M1[0][1] * M2[1][1] ;
-    M[1][0] =   M1[1][0] * M2[0][0] + M1[1][1] * M2[1][0] ;
-    M[1][1] =   M1[1][0] * M2[0][1] + M1[1][1] * M2[1][1] ;
+    MP[0][1] =   M1[0][0] * M2[0][1] + M1[0][1] * M2[1][1] ;
+    MP[0][0] =   M1[0][0] * M2[0][0] + M1[0][1] * M2[1][0] ;
+    MP[1][0] =   M1[1][0] * M2[0][0] + M1[1][1] * M2[1][0] ;
+    MP[1][1] =   M1[1][0] * M2[0][1] + M1[1][1] * M2[1][1] ;
 
 
-    // Printing the Product of the two arrays.
+    
+    int MA[rows][cols];         // MA - Matrix storing the Addition of M1 and M2.
+    int MS[rows][cols];         // MS - Matrix storing the Subtraction of M1 and M2.
+
+
+    // MA[0][0] = M1[0][0] + M2[0][0] ;
+    // MA[0][1] = M1[0][1] + M2[0][1] ;
+    // MA[1][0] = M1[1][0] + M2[1][0] ;
+    // MA[1][1] = M1[1][1] + M2[1][1] ;
+
+
+
+    // I'll be performing the same operation using a loop to make it more simple
+    
+
+    // Addition of two matrices
+    for(int i = 0; i < rows; i++){
+
+        for(int j = 0; j < cols; j++){
+
+            MA[i][j] = M1[i][j] + M2[i][j] ;
+        }
+    }
+
+    // Subtraction of two matrices
+    for(int i = 0; i < rows; i++){
+
+        for(int j = 0; j < cols; j++){
+
+            MS[i][j] = M1[i][j] - M2[i][j] ;
+        }
+    }
+
+
+    /*
+    Printing the Resulting Matrices. i.e. MP, MA, MS
+
+    MP - Product of M1, M2
+    MA - Addition of M1, M2
+    MS - Subtraction of M1, M2
+    */ 
+
+    // Printing the Matrix MP
+    cout<< "The product of the arrays is: " << endl;
     for (int i = 0; i < rows; i++){
 
         for(int j = 0; j < cols; j++){
 
-            cout<< M[i][j] << "\t";
+            cout<< MP[i][j] << "\t";
         }
 
         cout<< endl ;
     }
+
+    // Printing the Matrix MA
+    cout<< "The Addition of the arrays is: " << endl;
+    for (int i = 0; i < rows; i++){
+
+        for(int j = 0; j < cols; j++){
+
+            cout<< MA[i][j] << "\t";
+        }
+
+        cout<< endl ;
+    }
+
+    // Printing the Matrix MS
+    cout<< "The Subtraction of the arrays is: " << endl;
+    for (int i = 0; i < rows; i++){
+
+        for(int j = 0; j < cols; j++){
+
+            cout<< MS[i][j] << "\t";
+        }
+
+        cout<< endl ;
+    }
+
+
+
+
+
     
     return 0;
 
