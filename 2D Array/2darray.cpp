88815e2ed25b80580e470eade9eeb635e561b88b
Muhammad Huzaifa Ansar <huzaifaofficial087@gmail.com>
2025-11-19 19:51:06 +0500

    Loaded the remaining data

--- 2D Array/2darray.cpp
+++ 2D Array/2darray.cpp
@@ -25,11 +25,22 @@ int main(){
     // Product of the matrices would be stored in this matrix.
     int M[rows][cols];
 
-    M[0][0] =   M1[0][0] * M2[]
-    M[0][1] =
-    M[1][0] =
-    M[1][1] =
+    M[0][0] =   M1[0][0] * M2[0][0] + M1[0][1] * M2[1][0] ;
+    M[0][1] =   M1[0][0] * M2[0][1] + M1[0][1] * M2[1][1] ;
+    M[1][0] =   M1[1][0] * M2[0][0] + M1[1][1] * M2[1][0] ;
+    M[1][1] =   M1[1][0] * M2[0][1] + M1[1][1] * M2[1][1] ;
 
+
+    // Printing the Product of the two arrays.
+    for (int i = 0; i < rows; i++){
+
+        for(int j = 0; j < cols; j++){
+
+            cout<< M[i][j] << "\t";
+        }
+
+        cout<< endl ;
+    }
     
     return 0;
 
