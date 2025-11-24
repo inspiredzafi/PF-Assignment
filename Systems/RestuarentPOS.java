b158783edc3643e61d7cb2fb807bdc23961a4f24
Muhammad Huzaifa Ansar <huzaifaofficial087@gmail.com>
2025-11-25 00:18:06 +0500

    Finished the unfinished code for Restuarent POS system

--- Systems/RestuarentPOS.java
+++ Systems/RestuarentPOS.java
@@ -19,14 +19,45 @@ Use functions for each step.
 
 */
 
-class Order{
-    String name;
-    
+import java.util.Scanner;
+
+
+class Item{
+    public int prodID;
+    public String name;
+    public float price;
+
+    Item(String name, float price, int id){
+        prodID = id++ ;
+        this.name = name;
+        this.price = price;
+    }
+
+    public float getBill(int qty, float SST){
+        
+        float bill = price * qty;
+        float totalBill = bill + bill * (SST/100) ;
+
+        return totalBill;
+    }
+
 }
 
+
+
 public class RestuarentPOS {
+    
+    static Scanner scanner = new Scanner(System.in) ;
+    public static int id = 1;
 
     public static float SST = 8f;        // In percentage
+
+    public static Item Items[] = {
+        new Item("Mutton Paneer", 300, id),
+        new Item("Biryani", 250, id),
+        new Item("Pulao", 430, id),
+        new Item("Roti/Salan", 300, id)
+    } ;
 
     public static String Menu[] = {
             "1. Mutton Paneer", 
@@ -34,9 +65,25 @@ public class RestuarentPOS {
             "3. Pulao",
             "4. Roti/Salan"
         };
-    public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+    // Main Function
+    public static void main(String[] args) {
+        int choice;
+
+        do{
+
+            System.err.println("Welcome to Developement Restuarent");
+
+            displayMenu();
+            float bill = takeOrder();        // Takes order from the user and returns the total bill including the SST.
+            System.out.printf("\nThe total of your bill is %.2f (Including %%8 SST) \n", bill);
+
+
+            System.out.print("Want to order something more? Press Any key to continue (0 to exit) ");
+            choice = scanner.nextInt();
+        }while(choice != 0);
+
+        System.err.println("Thankyou for vising Developement Restuarent");
     }
 
     public static void displayMenu() {
@@ -45,5 +92,39 @@ public class RestuarentPOS {
             System.out.println(Menu[i]);
         }
 
+    }
+
+    public static float takeOrder(){
+
+        scanner.nextLine();     // clearing the buffer
+
+        System.out.print("Enter the name of dish: ");
+        String name = scanner.nextLine();
+        
+        System.out.print("Enter the quantity: ");
+        int qty = scanner.nextInt();
+        
+        System.err.println("Your order has been placed Sir! You'll be served shortly.");
+        
+        Item item = getItem(name) ;     // Extracting the item 
+        if(item == null){
+            System.out.println("Invalid dish name!");
+            return 0 ;
+        }
+
+        float bill = item.getBill(qty, SST);
+        
+        return bill;
+    }
+
+    public static Item getItem(String name){
+        for(int i = 0; i < Items.length; i++){
+            if(name.equals(Items[i].name)){
+
+                return Items[i];
+            }
+        }
+
+        return null;
     }
 }
